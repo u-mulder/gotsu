51568d46812a4447658fc1e4946411614807d970
u_mulder <m264695502@gmail.com>
2016-08-08 15:56:43 +0300

    - Fix cli sample  - Run fmt on main.go

--- main.go
+++ main.go
@@ -1,113 +1,111 @@
-
 package main
 
 import (
-    "fmt"
-    "flag"
-    "os"
-    "path/filepath"
-    "log"
-    "net/http"
-    "encoding/json"
-    "encoding/xml"
-    "github.com/PuerkitoBio/goquery"
-    "strings"
+	"encoding/json"
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 // Structs for testing via JSON config
 type configJsonEntity struct {
-    Protocol string             `json:"protocol"`
-    Domain string              `json:"domain"`
-    Urls []configJsonUrlEntity  `json:"urls"`
+	Protocol string                `json:"protocol"`
+	Domain   string                `json:"domain"`
+	Urls     []configJsonUrlEntity `json:"urls"`
 }
 
 type configJsonUrlEntity struct {
-    Url string                          `json:"url"`
-    StatusCode int                      `json:"statusCode"`
-    Elements []configJsonElementEntity  `json:"findElements"`
-
+	Url        string                    `json:"url"`
+	StatusCode int                       `json:"statusCode"`
+	Elements   []configJsonElementEntity `json:"findElements"`
 }
 
 type configJsonElementEntity struct {
-    Definition string         `json:"def"`
-    CountType string         `json:"countType"`
-    Count int                `json:"count"`
+	Definition string `json:"def"`
+	CountType  string `json:"countType"`
+	Count      int    `json:"count"`
 }
 
 // Structs for testing via XML config
 type configXmlEntity struct {
-    XMLName xml.Name        `xml:"urlset"`
-    Urls []SitemapXmlUrl    `xml:"url"`
+	XMLName xml.Name        `xml:"urlset"`
+	Urls    []SitemapXmlUrl `xml:"url"`
 }
 
 type SitemapXmlUrl struct {
-    Loc string  `xml:"loc"`
+	Loc string `xml:"loc"`
 }
 
 // Config file data struct
 type configData struct {
-    // relative path, where config file is stored
-    filePath string
-    // file type, currently allowed types: "json" and "sitemapxml"
-    fileType string
-    // flag which allows outputting more data while tests are run
-    verbose bool
+	// relative path, where config file is stored
+	filePath string
+	// file type, currently allowed types: "json" and "sitemapxml"
+	fileType string
+	// flag which allows outputting more data while tests are run
+	verbose bool
 }
 
 // Fill configData fields // TODO needs proper testing
 func (cd *configData) init() {
-    var config, filename, filetype, verbose string
-    flag.StringVar(&config, "config", "default", "Config name, default value is 'default'")
-    flag.StringVar(&filename, "filename", "conf", "Config file name w/o extension, 'conf' by default")
-    flag.StringVar(&filetype, "type", "json", "File type, current values 'json' and 'sitemapxml'")
-    flag.StringVar(&verbose, "verbose", "y", "Do not show messages for success tests")
-    flag.Parse()
+	var config, filename, filetype, verbose string
+	flag.StringVar(&config, "config", "default", "Config name, default value is 'default'")
+	flag.StringVar(&filename, "filename", "conf", "Config file name w/o extension, 'conf' by default")
+	flag.StringVar(&filetype, "type", "json", "File type, current values 'json' and 'sitemapxml'")
+	flag.StringVar(&verbose, "verbose", "y", "Do not show messages for success tests")
+	flag.Parse()
 
-    cd.fileType = "json"
-    if filetype == "sitemapxml" {
-        cd.fileType = "xml"
-        filename = "sitemap"
-    }
+	cd.fileType = "json"
+	if filetype == "sitemapxml" {
+		cd.fileType = "xml"
+		filename = "sitemap"
+	}
 
-    cd.filePath = fmt.Sprintf("/configs/%s/%s.%s", config, filename, cd.fileType)
-    cd.verbose = verbose == "y"
+	cd.filePath = fmt.Sprintf("/configs/%s/%s.%s", config, filename, cd.fileType)
+	cd.verbose = verbose == "y"
 }
 
 // Load finded file and instatiate required enitites
 func (cd *configData) load() {
-    curPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
-    if err != nil {
-        panic("/!\\ Unable to get current exec path")
-    }
+	curPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		panic("/!\\ Unable to get current exec path")
+	}
 
-    confFile := curPath + cd.filePath
-    if fileExists(confFile) {
-        file, err := os.Open(confFile)
-        if err != nil {
-            panic("/!\\ Error opening file " + confFile)
-        }
-        defer file.Close()
+	confFile := curPath + cd.filePath
+	if fileExists(confFile) {
+		file, err := os.Open(confFile)
+		if err != nil {
+			panic("/!\\ Error opening file " + confFile)
+		}
+		defer file.Close()
 
-        switch cd.fileType {
-            case typeJSON:
-                var decoder = json.NewDecoder(file)
-                err = decoder.Decode(&cJsonEntity)
-                if err != nil {
-                    panic("/!\\ Err decoding json file: " + err.Error())
-                }
-                cJsonEntity.runTests()
+		switch cd.fileType {
+		case typeJSON:
+			var decoder = json.NewDecoder(file)
+			err = decoder.Decode(&cJsonEntity)
+			if err != nil {
+				panic("/!\\ Err decoding json file: " + err.Error())
+			}
+			cJsonEntity.runTests()
 
-            case typeXML:
-                var decoder = xml.NewDecoder(file)
-                err = decoder.Decode(&cXmlEntity)
-                if err != nil {
-                    panic("/!\\ Err decoding json file: " + err.Error())
-                }
-                cXmlEntity.runTests()
-        }
-    } else {
-        panic("/!\\ Config file " + confFile + " not found ")
-    }
+		case typeXML:
+			var decoder = xml.NewDecoder(file)
+			err = decoder.Decode(&cXmlEntity)
+			if err != nil {
+				panic("/!\\ Err decoding json file: " + err.Error())
+			}
+			cXmlEntity.runTests()
+		}
+	} else {
+		panic("/!\\ Config file " + confFile + " not found ")
+	}
 
 }
 
@@ -116,19 +114,19 @@ var cJsonEntity = new(configJsonEntity)
 var cXmlEntity = new(configXmlEntity)
 
 const (
-    typeJSON = "json"
-    typeXML = "xml"
+	typeJSON = "json"
+	typeXML  = "xml"
 
-    EQ = "eq"
-    GT = "gt"
-    GTE = "gte"
-    LT = "lt"
-    LTE = "lte"
-    NE = "ne"
+	EQ  = "eq"
+	GT  = "gt"
+	GTE = "gte"
+	LT  = "lt"
+	LTE = "lte"
+	NE  = "ne"
 
-    statusCodeSuccessMsg = "Success. Requesting %s, expected status code %d confirmed\n"
-    statusCodeFailureMsg = "/!\\ Fail. Requesting %s, expected status code %d, got %d\n"
-    statusCodeSysFailMsg = "/!\\ SYSTEMFAIL. Error performing http-request to %s\n"
+	statusCodeSuccessMsg = "Success. Requesting %s, expected status code %d confirmed\n"
+	statusCodeFailureMsg = "/!\\ Fail. Requesting %s, expected status code %d, got %d\n"
+	statusCodeSysFailMsg = "/!\\ SYSTEMFAIL. Error performing http-request to %s\n"
 )
 
 /*
@@ -138,211 +136,211 @@ Available command line options
 -filename = custom name of config file, by default it's "conf", optional
 -verbose = output more data when tests are run, "n" by default, optional
 
-Example run: ./main -config=some_site -type=simplexml -verbose=n -filename=load.xml
+Example run: ./main -config=some_site -type=sitemapxml -verbose=n -filename=load
 */
 func main() {
-    cfData.init();
-    cfData.load();
+	cfData.init()
+	cfData.load()
 }
 
 func (ce *configJsonEntity) runTests() {
-    fullDomain := fmt.Sprintf("%s://%s", ce.Protocol, ce.Domain)
-    for _, v := range ce.Urls {
-        if "" != v.Url {
-            v.runTest(fullDomain)
-        }
-    }
+	fullDomain := fmt.Sprintf("%s://%s", ce.Protocol, ce.Domain)
+	for _, v := range ce.Urls {
+		if "" != v.Url {
+			v.runTest(fullDomain)
+		}
+	}
 
-    log.Printf("All tests for config '%s' completed", cfData.filePath)
+	log.Printf("All tests for config '%s' completed", cfData.filePath)
 }
 
 func (cue *configJsonUrlEntity) runTest(domain string) {
-    fullUrl := domain + cue.Url
-    reqType := "HEAD"
-    if 0 < len(cue.Elements) {
-        reqType = "GET"
-    }
+	fullUrl := domain + cue.Url
+	reqType := "HEAD"
+	if 0 < len(cue.Elements) {
+		reqType = "GET"
+	}
 
-    client := &http.Client{}
-    req, _ := http.NewRequest(reqType, fullUrl, nil)
-    resp, err := client.Do(req)
+	client := &http.Client{}
+	req, _ := http.NewRequest(reqType, fullUrl, nil)
+	resp, err := client.Do(req)
 
-    if err == nil {
-        if resp.StatusCode == cue.StatusCode {
-            if cfData.verbose {
-                printMsg(fmt.Sprintf(statusCodeSuccessMsg, fullUrl, cue.StatusCode))
-            }
+	if err == nil {
+		if resp.StatusCode == cue.StatusCode {
+			if cfData.verbose {
+				printMsg(fmt.Sprintf(statusCodeSuccessMsg, fullUrl, cue.StatusCode))
+			}
 
-            if 0 < len(cue.Elements) {
-                doc, err := goquery.NewDocumentFromResponse(resp)
-                if err == nil {
-                    for _, v := range cue.Elements {
-                        v.testElement(doc)
-                    }
-                } else {
-                    printMsg(fmt.Sprintf("/!\\ SYSTEMFAIL. Error reading http-request body from %s\n", fullUrl))
-                }
-            }
-        } else {
-            printMsg(fmt.Sprintf(statusCodeFailureMsg, fullUrl, cue.StatusCode, resp.StatusCode))
-        }
-    } else {
-        printMsg(fmt.Sprintf(statusCodeSysFailMsg, fullUrl))
-    }
+			if 0 < len(cue.Elements) {
+				doc, err := goquery.NewDocumentFromResponse(resp)
+				if err == nil {
+					for _, v := range cue.Elements {
+						v.testElement(doc)
+					}
+				} else {
+					printMsg(fmt.Sprintf("/!\\ SYSTEMFAIL. Error reading http-request body from %s\n", fullUrl))
+				}
+			}
+		} else {
+			printMsg(fmt.Sprintf(statusCodeFailureMsg, fullUrl, cue.StatusCode, resp.StatusCode))
+		}
+	} else {
+		printMsg(fmt.Sprintf(statusCodeSysFailMsg, fullUrl))
+	}
 }
 
-func (cee *configJsonElementEntity) testElement (doc *goquery.Document) {
-    elDefinition := strings.TrimSpace(cee.Definition)
-    elCouType := strings.TrimSpace(cee.CountType)
-    selection := doc.Find( elDefinition )
-    msg := fmt.Sprintf("/!\\ Fail. Not supported elCouType %s\n", elCouType)
+func (cee *configJsonElementEntity) testElement(doc *goquery.Document) {
+	elDefinition := strings.TrimSpace(cee.Definition)
+	elCouType := strings.TrimSpace(cee.CountType)
+	selection := doc.Find(elDefinition)
+	msg := fmt.Sprintf("/!\\ Fail. Not supported elCouType %s\n", elCouType)
 
-    selLen := selection.Length()
-    switch elCouType {
-        case EQ:
-            if selLen == cee.Count {
-                if cfData.verbose {
-                    msg = getSelectorTestSuccMsg(elDefinition, EQ, cee.Count)
-                } else {
-                    msg = "";
-                }
-            } else {
-                msg = getSelectorTestFailMsg(elDefinition, EQ, cee.Count, selLen)
-            }
+	selLen := selection.Length()
+	switch elCouType {
+	case EQ:
+		if selLen == cee.Count {
+			if cfData.verbose {
+				msg = getSelectorTestSuccMsg(elDefinition, EQ, cee.Count)
+			} else {
+				msg = ""
+			}
+		} else {
+			msg = getSelectorTestFailMsg(elDefinition, EQ, cee.Count, selLen)
+		}
 
-        case GT:
-            if selLen > cee.Count {
-                if cfData.verbose {
-                    msg = getSelectorTestSuccMsg(elDefinition, GT, cee.Count)
-                } else {
-                    msg = "";
-                }
-            } else {
-                msg = getSelectorTestFailMsg(elDefinition, GT, cee.Count, selLen)
-            }
+	case GT:
+		if selLen > cee.Count {
+			if cfData.verbose {
+				msg = getSelectorTestSuccMsg(elDefinition, GT, cee.Count)
+			} else {
+				msg = ""
+			}
+		} else {
+			msg = getSelectorTestFailMsg(elDefinition, GT, cee.Count, selLen)
+		}
 
-        case GTE:
-            if selLen >= cee.Count {
-                if cfData.verbose {
-                    msg = getSelectorTestSuccMsg(elDefinition, GTE,  cee.Count)
-                } else {
-                    msg = "";
-                }
-            } else {
-                msg = getSelectorTestFailMsg(elDefinition, GTE, cee.Count, selLen)
-            }
+	case GTE:
+		if selLen >= cee.Count {
+			if cfData.verbose {
+				msg = getSelectorTestSuccMsg(elDefinition, GTE, cee.Count)
+			} else {
+				msg = ""
+			}
+		} else {
+			msg = getSelectorTestFailMsg(elDefinition, GTE, cee.Count, selLen)
+		}
 
-        case LT:
-            if selLen < cee.Count {
-                if cfData.verbose {
-                    msg = getSelectorTestSuccMsg(elDefinition, LT, cee.Count)
-                } else {
-                    msg = "";
-                }
-            } else {
-                msg = getSelectorTestFailMsg(elDefinition, LT, cee.Count, selLen)
-            }
+	case LT:
+		if selLen < cee.Count {
+			if cfData.verbose {
+				msg = getSelectorTestSuccMsg(elDefinition, LT, cee.Count)
+			} else {
+				msg = ""
+			}
+		} else {
+			msg = getSelectorTestFailMsg(elDefinition, LT, cee.Count, selLen)
+		}
 
-        case LTE:
-            if selLen <= cee.Count {
-                if cfData.verbose {
-                    msg = getSelectorTestSuccMsg(elDefinition, LTE, cee.Count)
-                } else {
-                    msg = "";
-                }
-            } else {
-                msg = getSelectorTestFailMsg(elDefinition, LTE, cee.Count, selLen)
-            }
+	case LTE:
+		if selLen <= cee.Count {
+			if cfData.verbose {
+				msg = getSelectorTestSuccMsg(elDefinition, LTE, cee.Count)
+			} else {
+				msg = ""
+			}
+		} else {
+			msg = getSelectorTestFailMsg(elDefinition, LTE, cee.Count, selLen)
+		}
 
-        case NE:
-            if selLen != cee.Count {
-                if cfData.verbose {
-                    msg = getSelectorTestSuccMsg(elDefinition, NE, cee.Count)
-                } else {
-                    msg = "";
-                }
-            } else {
-                msg = getSelectorTestFailMsg(elDefinition, NE, cee.Count, selLen)
-            }
-    }
+	case NE:
+		if selLen != cee.Count {
+			if cfData.verbose {
+				msg = getSelectorTestSuccMsg(elDefinition, NE, cee.Count)
+			} else {
+				msg = ""
+			}
+		} else {
+			msg = getSelectorTestFailMsg(elDefinition, NE, cee.Count, selLen)
+		}
+	}
 
-    if "" != msg {
-        printMsg(msg)
-    }
+	if "" != msg {
+		printMsg(msg)
+	}
 }
 
 func (ce *configXmlEntity) runTests() {
-    for _, v := range ce.Urls {
-        if "" != v.Loc {
-            v.runTest()
-        }
-    }
+	for _, v := range ce.Urls {
+		if "" != v.Loc {
+			v.runTest()
+		}
+	}
 
-    log.Printf("All tests for config '%s' completed", cfData.filePath)
+	log.Printf("All tests for config '%s' completed", cfData.filePath)
 }
 
 func (sxu *SitemapXmlUrl) runTest() {
-    reqType := "HEAD"
-    client := &http.Client{}
-    req, _ := http.NewRequest(reqType, sxu.Loc, nil)
-    resp, err := client.Do(req)
-    /* consider that all urls in sitemap shoud give 200 status code */
-    requiredStatusCode := 200
+	reqType := "HEAD"
+	client := &http.Client{}
+	req, _ := http.NewRequest(reqType, sxu.Loc, nil)
+	resp, err := client.Do(req)
+	/* consider that all urls in sitemap shoud give 200 status code */
+	requiredStatusCode := 200
 
-    if err == nil {
-        if resp.StatusCode == requiredStatusCode {
-            if cfData.verbose {
-                printMsg(fmt.Sprintf(statusCodeSuccessMsg, sxu.Loc, requiredStatusCode))
-            }
-        } else {
-            printMsg(fmt.Sprintf(statusCodeFailureMsg, sxu.Loc, requiredStatusCode, resp.StatusCode))
-        }
-    } else {
-        printMsg(fmt.Sprintf(statusCodeSysFailMsg, sxu.Loc))
-    }
+	if err == nil {
+		if resp.StatusCode == requiredStatusCode {
+			if cfData.verbose {
+				printMsg(fmt.Sprintf(statusCodeSuccessMsg, sxu.Loc, requiredStatusCode))
+			}
+		} else {
+			printMsg(fmt.Sprintf(statusCodeFailureMsg, sxu.Loc, requiredStatusCode, resp.StatusCode))
+		}
+	} else {
+		printMsg(fmt.Sprintf(statusCodeSysFailMsg, sxu.Loc))
+	}
 }
 
 func printMsg(msg string) {
-    prLine(false)
-    log.Printf(msg)
-    prLine(true)
+	prLine(false)
+	log.Printf(msg)
+	prLine(true)
 }
 
 func prLine(doubleNl bool) {
-    var nl = "\n"
-    if doubleNl {
-        nl = "\n\n"
-    }
-    fmt.Print("---------------------------" + nl)
+	var nl = "\n"
+	if doubleNl {
+		nl = "\n\n"
+	}
+	fmt.Print("---------------------------" + nl)
 }
 
 func getSelectorTestSuccMsg(elDef string, couType string, cou int) string {
-    return  fmt.Sprintf(
-        "Success. Selector: '%s'. Expected size '%s %d' confirmed\n",
-        elDef,
-        couType,
-        cou,
-    )
+	return fmt.Sprintf(
+		"Success. Selector: '%s'. Expected size '%s %d' confirmed\n",
+		elDef,
+		couType,
+		cou,
+	)
 }
 
 func getSelectorTestFailMsg(elDef string, couType string, exCou int, realCou int) string {
-    return fmt.Sprintf(
-        "/!\\ Fail. Selector: '%s'. Expected size '%s %d', received size %d\n",
-        elDef,
-        couType,
-        exCou,
-        realCou,
-    )
+	return fmt.Sprintf(
+		"/!\\ Fail. Selector: '%s'. Expected size '%s %d', received size %d\n",
+		elDef,
+		couType,
+		exCou,
+		realCou,
+	)
 }
 
 func fileExists(fileName string) bool {
-    result := true
+	result := true
 
-    if _, err := os.Stat(fileName); err != nil {
-        if os.IsNotExist(err) {
-            result = false
-        }
-    }
+	if _, err := os.Stat(fileName); err != nil {
+		if os.IsNotExist(err) {
+			result = false
+		}
+	}
 
-    return result
+	return result
 }
